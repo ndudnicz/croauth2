19d8049fd18f94a6b45f7aff09aea511f4c9324a
ndudniczenko <ndudniczenko@vente-privee.com>
2019-03-25 17:49:51 +0100

    update readme (examples, prototypes ...), better exception handling

--- MyOauth.cr
+++ MyOauth.cr
@@ -1,7 +1,7 @@
 require "http/client"
 require "json"
 
-class MyOauth
+class Myoauth
   struct Token
     include JSON::Serializable
     getter access_token : String = String.new
@@ -11,6 +11,7 @@ class MyOauth
 
   @token = Token.from_json(%({}))
   @auth_header = String.new
+  @_http_client = HTTP::Client.new("")
 
   def initialize(
     endpoint : String,
@@ -20,11 +21,12 @@ class MyOauth
     match = endpoint.match(/^(?<ht>https?):\/\/(?<uri>[^:]+)(:(?<port>[0-9]+))?$/).not_nil!
     @_client_id = client_id
     @_client_secret = client_secret
-    @http_client = HTTP::Client.new(
+    @_http_client = HTTP::Client.new(
       host: match.named_captures["uri"].to_s,
       port: match.named_captures["port"] ? match.named_captures["port"].to_s : nil,
       tls: match.named_captures["ht"].to_s == "https" ? true : false
-    )
+    ).not_nil!
+    @endpoint = endpoint
   end
 
   def get_token_from_credentials
@@ -33,14 +35,20 @@ class MyOauth
       "client_id" => @_client_id,
       "client_secret" => @_client_secret
     })
-    res = @http_client.post("/oauth/token?" + params)
-    body_json = JSON.parse(res.body)
+    res = @_http_client.post("/oauth/token?" + params)
+    res.not_nil!
+    begin
+      body_json = JSON.parse(res.body)
+    rescue ex
+      puts res.body
+      puts "Invalid end point : " + @endpoint + " ?"
+      raise "Error: " + ex.message.to_s
+    end
     @token = Token.from_json(res.body)
 
-    @http_client.before_request do |request|
+    @_http_client.before_request do |request|
       request.headers["Authorization"] = "Bearer " + @token.@access_token
     end
-
   end
 
   # TODO: handle params, improve returned value
@@ -48,17 +56,8 @@ class MyOauth
     path : String,
     params  = nil
   )
-    @http_client.get(
+    @_http_client.get(
       path
     )
   end
 end
-
-
-# CLIENT_ID = ENV["CLIENT_ID"]
-# CLIENT_SECRET = ENV["CLIENT_SECRET"]
-#
-# o = MyOauth.new "https://api.intra.42.fr", CLIENT_ID, CLIENT_SECRET
-# o.get_token_from_credentials()
-# res = o.get("/v2/users", params: {"page" => { "number" => "2" } })
-# puts res.body
--- README.md
+++ README.md
@@ -1,13 +1,33 @@
+
 An oauth module in crystal
 
 ### Requierment:
 * crystal >= 0.27.2
 
-### HOW TO
-```bash
-$> npm install
-$> node index.js
+### Example
+```ruby
+require "./Myoauth"
+
+CLIENT_ID = ENV["CLIENT_ID"]
+CLIENT_SECRET = ENV["CLIENT_SECRET"]
+
+o = Myoauth.new "https://api.intra.42.fr", CLIENT_ID, CLIENT_SECRET
+o.get_token_from_credentials()
+res = o.get("/v2/users", params: {"page" => { "number" => "2" } })
+puts res.body
 ```
 
-### API DOCUMENTATION
-[https://api.intra.42.fr/apidoc](https://api.intra.42.fr/apidoc)
+### API
+#### Constructor
+```crystal
+.new(endpoint: String, client_id: String, client_secret: String)
+```
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;create a new oauth client
+
+#### Instance Methods Summary
+
+```crystal
+.get(path: String, params : Hash(String, String) = nil) :
+HTTP::Client::Response
+```
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Executes a GET request with the params passed as url encoded parameters (WIP). Returns [HTTP::Client::Response](https://crystal-lang.org/api/0.27.2/HTTP/Client/Response.html)
